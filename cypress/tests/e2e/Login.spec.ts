b60b49103a6e0139b2368521faa9fd7f2f78ce92
Yehudi01 <yehudi21903@estudante.se.df.gov.br>
2024-11-06 12:39:07 -0300

    added login fail page

--- cypress/tests/e2e/Login.spec.ts
+++ cypress/tests/e2e/Login.spec.ts
@@ -6,3 +6,13 @@ describe('Login com sucesso', () => {
     cy.get("[type='submit']").click()
   })
 })
+
+describe('Tentar fazer login com credenciais inválidas', () => {
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get("[type='text']").type("test")
+    cy.get("[type='password']").type("s3cret")
+    cy.get("[type='submit']").click()
+    cy.get("[data-test='signin-error']").should('contain', 'Username or password is invalid')
+  });
+});
